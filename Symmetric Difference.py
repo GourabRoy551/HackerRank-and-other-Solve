d47011b3dc2cda2ae7bcb052c1e77740793635fb
GourabRoy551 <gourabroysec553@gmail.com>
2021-02-16 13:40:10 +0600

    Symmetric Differemce

---
+++ Symmetric Difference.py
@@ -0,0 +1,23 @@
+from pip._vendor.distlib.compat import raw_input
+
+r = int(raw_input())
+set_a_str_ar = raw_input().strip().split()
+set_a_ar = list(map(int, set_a_str_ar))
+
+n = int(raw_input())
+set_b_str_ar = raw_input().strip().split()
+set_b_ar = list(map(int,set_b_str_ar))
+
+
+set_a_set = set(set_a_ar)
+set_b_set = set(set_b_ar)
+
+set_a_dif_set = set_a_set.difference(set_b_set)
+set_b_dif_set = set_b_set.difference(set_a_set)
+
+res_set = set_a_dif_set.union(set_b_dif_set)
+res_ar = list(res_set)
+res_ar.sort()
+
+for i in res_ar:
+    print(i)
