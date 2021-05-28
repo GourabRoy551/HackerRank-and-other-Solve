9bed823884fbc626a9a06d68781b07d31cc30fd1
GourabRoy551 <gourabroysec553@gmail.com>
2021-05-28 11:21:56 +0600

    pycode

---
+++ itertools.permutations() .py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+s, n = input().split()
+
+# print(*[''.join(i) for i in permutations(sorted(s), int(n))], sep='\n')
+
+for i in permutations(sorted(s), int(n)):
+    print(*[''.join(i)])
